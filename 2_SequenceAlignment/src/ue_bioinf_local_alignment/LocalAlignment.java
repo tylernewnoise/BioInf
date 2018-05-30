c978796bbb08e13e8717563d13e88e1876cbf5ba
Falko <falko.becker@informatik.hu-berlin.de>
2018-05-30 23:12:32 +0200

    removed unused import

--- 2_SequenceAlignment/src/ue_bioinf_local_alignment/LocalAlignment.java
+++ 2_SequenceAlignment/src/ue_bioinf_local_alignment/LocalAlignment.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
